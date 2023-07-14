e07ef14b237b238b08808d8a4b9f1229151eba46
Asjad Raja <mac@Asjads-MacBook-Pro.local>
2023-07-14 18:02:05 +0500

    Static API call

---
+++ data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    name: "Tester",
+    email: "test@email.com",
+    address: "street2,lane2,block2,city,country",
+  },
+  {
+    name: "Tester 2",
+    email: "test2@email.com",
+    address: "street2,lane2,block2,city,country",
+  },
+  {
+    name: "Tester 3",
+    email: "test3@email.com",
+    address: "street2,lane2,block2,city,country",
+  },
+  {
+    name: "Tester 4",
+    email: "test4@email.com",
+    address: "street2,lane2,block2,city,country",
+  },
+];
+
+module.exports = data;
--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-var http = require("http");
+const http = require("http");
+const data = require("./data");
+
+let count = 5;
+
+const countdown = setInterval(() => {
+  console.log(`The server is Booting up in ${count} seconds...`);
+  count--;
+  if (count === 0) {
+    clearInterval(countdown);
+    console.log("Loading...");
+    setTimeout(() => {
+      console.log("Server running at port: 5000");
+    }, 2000);
+  }
+}, 1000);
+
 http
   .createServer(function (request, response) {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Hello World");
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.write(JSON.stringify(data));
+    response.end();
   })
-  .listen(3000);
-
-console.log("Server running at port: 3000");
+  .listen(5000);
